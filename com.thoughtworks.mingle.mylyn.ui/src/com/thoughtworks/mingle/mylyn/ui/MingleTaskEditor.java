0e60b2c9b15ba14574a15b2f6e28e95b2b272cb2
kpadegao <kpadegao@1906d8cc-e22d-42bc-8caf-f3674d5a0c11>
2008-01-16 09:29:52 +0000

    - added NO-OP form support.

--- com.thoughtworks.mingle.mylyn.ui/src/com/thoughtworks/mingle/mylyn/ui/MingleTaskEditor.java
+++ com.thoughtworks.mingle.mylyn.ui/src/com/thoughtworks/mingle/mylyn/ui/MingleTaskEditor.java
@@ -1,10 +1,17 @@
 package com.thoughtworks.mingle.mylyn.ui;
 
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.mylyn.tasks.core.RepositoryTaskAttribute;
 import org.eclipse.mylyn.tasks.ui.editors.AbstractRepositoryTaskEditor;
 import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
-public class MingleTaskEditor extends AbstractRepositoryTaskEditor{
-
+public class MingleTaskEditor extends AbstractRepositoryTaskEditor {
 
     public MingleTaskEditor(TaskEditor editor) {
         super(editor);
@@ -12,7 +19,27 @@ public class MingleTaskEditor extends AbstractRepositoryTaskEditor{
 
     @Override
     protected void validateInput() {
-        
+
     }
-    
+
+    protected void createCustomAttributeLayout(Composite composite) {
+        RepositoryTaskAttribute attribute = new RepositoryTaskAttribute("id", "name", false);
+
+        Label label = createLabel(composite, attribute);
+        GridDataFactory.fillDefaults().align(SWT.RIGHT, SWT.CENTER).applyTo(label);
+        Composite textFieldComposite = getManagedForm().getToolkit().createComposite(composite);
+        GridLayout textLayout = new GridLayout();
+        textLayout.marginWidth = 1;
+        textLayout.marginHeight = 3;
+        textLayout.verticalSpacing = 3;
+        textFieldComposite.setLayout(textLayout);
+        GridData textData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+        textData.horizontalSpan = 1;
+        textData.widthHint = 135;
+
+        final Text text = createTextField(textFieldComposite, attribute, SWT.FLAT);
+        text.setLayoutData(textData);
+        getManagedForm().getToolkit().paintBordersFor(textFieldComposite);
+    }
+
 }
